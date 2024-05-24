31f37902a418f74e71dfb7f2c529c5d629c38b76
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-24 18:01:28 -0300

    fix: removendo quebra de linhas desnecessárias

--- backend/src/useCases/gameUseCases.py
+++ backend/src/useCases/gameUseCases.py
@@ -49,8 +49,7 @@ class GameUseCases():
     game_positions = [a for a in gameData]
     free_positions_indexes = []
     for index, position in enumerate(game_positions):
-      if position == " ":
-        free_positions_indexes.append(index)
+      if position == " ": free_positions_indexes.append(index)
     move_position_index = random.randint(0, len(free_positions_indexes)-1)
     return free_positions_indexes[move_position_index]
     
@@ -70,8 +69,7 @@ class GameUseCases():
     )  
     
     result = self.verify_result(game["data"])
-    if not result is None:
-      return await self.handle_result(game, result)
+    if not result is None: return await self.handle_result(game, result)
     
     self.game_memory.running_games[game["id"]]["current"] = "x" if game["current"]=="o" else "o"
   
@@ -88,8 +86,7 @@ class GameUseCases():
     for a, b, c in possibles_win_positions:
       has_winner = game_data[a] == game_data[b] and game_data[b] \
         == game_data[c] and game_data[c] != " "
-      if has_winner:
-        break 
+      if has_winner: break 
       
     if has_winner: return "win" 
     if blank_positions == 0: return "tie"
@@ -110,36 +107,30 @@ class GameUseCases():
     except ValueError:
       return await self.sio.emit("bad", "a posição precisa ser um inteiro", to=sid)
     
-    if position is None or (position < 0 or position > 9):
-      return await self.sio.emit("bad", "posição invalida", to=sid)
+    if position is None or (position < 0 or position > 9): return await self.sio.emit("bad", "posição invalida", to=sid)
     
     game_on_memory = self.game_memory.running_games.get(game_id, None) 
-    if game_on_memory is None:
-      return await self.sio.emit("bad", "id de partida invalida", to=sid)
+    if game_on_memory is None: return await self.sio.emit("bad", "id de partida invalida", to=sid)
     
     player_is_x = game_on_memory["x_player_sid"]==sid
     player_is_o = game_on_memory["o_player_sid"]==sid
-    if not player_is_x and not player_is_o:
-      return await self.sio.emit("bad", "a partida informada não é sua", to=sid)
+    if not player_is_x and not player_is_o: return await self.sio.emit("bad", "a partida informada não é sua", to=sid)
 
     if (player_is_x and game_on_memory["current"] != "x") or (player_is_o and game_on_memory["current"] != "o"):
       return await self.sio.emit("bad", "Não é a sua vez de jogar", to=sid)
     
     game_data = list(game_on_memory["data"])
-    if game_data[position] != " ":
-      return await self.sio.emit("bad", "posição invalida", to=sid)
+    if game_data[position] != " ": return await self.sio.emit("bad", "posição invalida", to=sid)
     
     game_data[position] = game_on_memory["current"]
     new_game_data = ''.join(game_data)
     game_on_memory["data"] = new_game_data
     
     result = self.verify_result(game_on_memory["data"])
-    if not result is None:
-      return await self.handle_result(game_on_memory, result)
+    if not result is None: return await self.handle_result(game_on_memory, result)
     
     game_on_memory["current"] = "x" if game_on_memory["current"]=="o" else "o"  
-    if game_on_memory["mode"] == "algoritmo":
-      await self.algorithm_move(game_on_memory)
+    if game_on_memory["mode"] == "algoritmo": await self.algorithm_move(game_on_memory)
       
   def delete_game(self, sid=None, game=None):
     if game is None:
@@ -148,6 +139,7 @@ class GameUseCases():
           self.game_memory.running_games[game_id]["o_player_sid"] == sid):
             game = self.game_memory.running_games[game_id]
             break
+          
     if game.get("x_player_sid") is not None: del self.game_memory.players_in_game[game["x_player_sid"]]
     if game.get("o_player_sid") is not None: del self.game_memory.players_in_game[game["o_player_sid"]]
     del game
