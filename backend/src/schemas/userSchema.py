aea980ec86dc217320ed5a2fd7b4e04a0fe6b79b
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-28 18:28:24 -0300

    refactor: renomeando parametros dos validadores de campo

--- backend/src/schemas/userSchema.py
+++ backend/src/schemas/userSchema.py
@@ -13,22 +13,21 @@ class UserSchema(BaseModel):
   
   @field_validator("username")
   @staticmethod
-  def validate_username(value):
-    if not re.match("^([a-z]|[A-Z]|[0-9])+$", value) or len(value) > 10:
+  def validate_username(username):
+    if not re.match("^([a-z]|[A-Z]|[0-9])+$", username) or len(username) > 10:
       raise HTTPException(
         detail="Nome de usuário invalido",
         status_code=status.HTTP_400_BAD_REQUEST
       )
-    return value
+    return username
   
   @field_validator("email")
   @staticmethod
-  def validate_email(value):
+  def validate_email(email):
     validate_email_regex = r"""^(?:(?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*)|(?:".+"))@(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}|(?:\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\]))$"""
-    if not re.match(validate_email_regex, value):
+    if not re.match(validate_email_regex, email):
       raise HTTPException(
         detail="email invalido",
         status_code=status.HTTP_400_BAD_REQUEST
       )
-    return value
-    
+    return email
