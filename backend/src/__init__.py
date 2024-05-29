cac8f9eaab26e0dc2ebe2ebf4287c283b6f5af78
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-28 21:09:31 -0300

    feat: configurando cors

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes import authentication_router, authenticated_router, global_router
 from .socketio import socketio_app
@@ -6,6 +7,13 @@ from .socketio import socketio_app
 
 #Fastapi settings
 app = FastAPI()
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 app.include_router(authentication_router)
 app.include_router(authenticated_router)
 app.include_router(global_router)
