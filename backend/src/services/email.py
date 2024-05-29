fd414c0cd1d92c00b4f11f04c99cd01c8101851c
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2024-05-29 12:14:10 -0300

    fix: resolvendo má uso que limitava o serviço em apenas 1 email

--- backend/src/services/email.py
+++ backend/src/services/email.py
@@ -4,11 +4,12 @@ from email.mime.multipart import MIMEMultipart
 class EmailService():
 
   def __init__(self, email:str, password:str):
-      smtp = smtplib.SMTP_SSL("smtp.gmail.com")
-      smtp.connect("smtp.gmail.com")
-      smtp.login(email, password)
-      self.smtp = smtp
-      self.address = email
+    self.smtp = smtplib.SMTP_SSL("smtp.gmail.com")
+    self.address = email
+    self.password = password
 
   def send_email(self, msg:MIMEMultipart):
+    self.smtp.connect("smtp.gmail.com")
+    self.smtp.login(self.address, self.password)
     self.smtp.send_message(msg)
+    self.smtp.quit()
